856427d0f4934035e7d127d1bd68c8e34ade960b
neuzenir <115902927+neuzenir@users.noreply.github.com>
2025-02-27 18:02:46 -0300

    Update super_trunfo.c
    Adicionada a comparação

--- super_trunfo.c
+++ super_trunfo.c
@@ -10,7 +10,7 @@ int main() {
     char codigo1[4], codigo2[4];
     char cidade1[20], cidade2[20];
     int populacao1, populacao2, pontos1, pontos2;
-    float area1, area2, pib1, pib2, densidade1, densidade2, percapta1, percapta2;
+    float area1, area2, pib1, pib2, densidade1, densidade2, percapta1, percapta2, inverso_densidade1, inverso_densidade2;
 
        // Cadastro das Cartas:
     
@@ -66,6 +66,8 @@ int main() {
     densidade2 = (float)populacao2 / area2;
     percapta1 = pib1 / (float) populacao1;
     percapta2 = pib2 / (float)populacao2;
+    inverso_densidade1 = 1 / densidade1;
+    inverso_densidade2 = 1 / densidade2;
 
     // Imprime na tela as informações cadastradas.
         printf ("\nDados da Carta 1: \n");
@@ -90,5 +92,16 @@ int main() {
         printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
         printf("PIB per Capta: %.2f reais\n", percapta2);
 
-    return 0;
-}
+        //Comparação entre as cartas e resultado.
+
+        printf ("\nComparação das Cartas (Atributo: População): \n");
+        printf("Carta 1 - %s, %d\n", cidade1, populacao1);
+        printf("Carta 2 - %s, %d\n", cidade2, populacao2);
+
+        if (populacao1 > populacao2)
+        {
+          printf("Resultado: Carta 1 - %s Venceu!\n", cidade1);
+        } else {
+          printf("Resultado: Carta 2 - %s Venceu!\n", cidade2);
+        }
+
